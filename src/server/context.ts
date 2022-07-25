9dce06010f1f596c676417b7e989c7d5d3831837
ZiiM <alextede8899@gmail.com>
2022-07-25 01:06:35 -0400

    Attemp at fixing trpc on vercel

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,13 +1,14 @@
 import { prisma } from '@/utils/database';
 import * as trpc from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
-import { getSession } from 'next-auth/react';
+import { unstable_getServerSession } from 'next-auth';
+import { authOptions } from 'src/pages/api/auth/[...nextauth]';
 
 export const createContext = async ({
   req,
   res,
 }: trpcNext.CreateNextContextOptions) => {
-  const session = await getSession({ req });
+  const session = await unstable_getServerSession(req, res, authOptions);
   console.log('createContext for', session?.user?.name ?? 'unknown user');
   return {
     req,
